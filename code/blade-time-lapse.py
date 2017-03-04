54ea4bcb200808ac63fcf79ec2912f249a20c2c3
Jon Witts <jonwitts@gmail.com>
2017-03-04 15:11:57 +0000

    First version of Python code

--- code/blade-time-lapse.py
+++ code/blade-time-lapse.py
@@ -1,1 +1,18 @@
 #!/bin/python3
+
+from time import sleep
+from gpiozero import LED
+from picamera import PiCamera
+from os import system
+
+cam = PiCamera()
+led = LED(2)
+
+system('mkdir /home/pi/captures')
+
+led.on()
+
+for i in range(10):
+    cam.capture('/home/pi/captures/image{0:07d}.jpg'.format(i))
+led.off()
+
