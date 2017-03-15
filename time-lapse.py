60ddeead52b20503d79dc6b93496e468c736df12
Jon Witts <jonwitts@users.noreply.github.com>
2017-03-15 20:13:16 +0000

    Update and rename code/blade-time-lapse.py to time-lapse.py

--- time-lapse.py
+++ time-lapse.py
@@ -3,16 +3,14 @@
 from time import sleep
 from gpiozero import LED
 from picamera import PiCamera
-from os import system
 
 cam = PiCamera()
 led = LED(2)
-
-system('mkdir /home/pi/captures')
 
 led.on()
 
 for i in range(10):
     cam.capture('/home/pi/captures/image{0:07d}.jpg'.format(i))
+    sleep(5)
 led.off()
 
