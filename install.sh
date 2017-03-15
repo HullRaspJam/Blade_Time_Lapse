5c4484d6923aaf16b716e03142cd4c46eb098e49
Jon Witts <jonwitts@users.noreply.github.com>
2017-03-15 20:18:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
- mkdir /home/pi/timelapse/
- mkdir /home/pi/timelapse/captures/
+mkdir /home/pi/timelapse/
+mkdir /home/pi/timelapse/captures/
+
+cp ./code/time-lapse.py /home/pi/timelapse/
+sudo cp ./code/timelapse.service /lib/systemd/system/
+
+sudo chmod 644 /lib/systemd/system/timelapse.service
+sudo chmod +x /home/pi/timelapse/time-lapse.py
+
+sudo systemctl daemon-reload
+sudo systemctl enable timelapse.service
+
+sudo reboot now
